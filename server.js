6286fb8b8f3c5a326fa22347cc75c12f5d429c81
Rachel La <rachel.la1807@gmail.com>
2023-07-01 12:58:21 -0400

    transition from sign up to sign in

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-    console.log('request made');
-});
+let title = document.getElementById("title");
+let email = document.getElementById("email");
+let signupBtn = document.getElementById("signup-btn");
+let signinBtn = document.getElementById("signin-btn");
+let question = document.getElementById("question");
 
-server.listen(3000, 'localhost', () => {
-    console.log('listening for requests on port 3000')
-});
+var flag = true;
+
+signinBtn.onclick = function() {
+    if (flag) {
+        title.innerHTML = "Sign in"
+        email.style.maxHeight = "0";
+        signupBtn.innerHTML = "Sign in";
+        question.innerHTML = "Don't have an account?"
+        signinBtn.innerHTML = "Sign up";
+        flag = false;
+    } else {
+        title.innerHTML = "Sign up"
+        email.style.maxHeight = "60px";
+        signupBtn.innerHTML = "Sign up";
+        question.innerHTML = "Already have an account?"
+        signinBtn.innerHTML = "Sign in";
+        flag = true;
+    }
+    
+}
+
+signupBtn.onclick = function() {
+    email.style.maxHeight = "60px";
+}
