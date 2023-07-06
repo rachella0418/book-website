adddddd01171c115e824ab3911e62ab20abf2e8d
Rachel La <rachel.la1807@gmail.com>
2023-07-06 15:31:15 -0400

    fixed error where there arent enough books to fit all in a row

--- public/app.js
+++ public/app.js
@@ -9,6 +9,8 @@ $(document).ready(function() {
         $("#mylib-field").css("visibility", "hidden");
         $("#topread-field").css("visibility", "hidden");
         $(".book-list").css("visibility", "visible");
+        $("#search-message").css("visibility", "hidden");
+        $(".book-list").css("margin-top", "0");
         outputList.innerHTML = "";
         //document.body.style.backgroundImage - "url('')";
         searchData = $(".search").val();
@@ -40,38 +42,50 @@ $(document).ready(function() {
     });
 
     function displayResults(res) {
-        for (var i = 0; i < res.items.length; i += 5) {
+        for (var i = 0; i < res.items.length; i += 4) {
+            var row = '<div id="row">'
             item1 = res.items[i];
             title1 = item1.volumeInfo.title;
             author1 = item1.volumeInfo.authors;
             cover1 = (item1.volumeInfo.imageLinks) ? item1.volumeInfo.imageLinks.thumbnail : placeholder;
+            row += formatOutput(cover1, title1, author1);
+            if (res.items[i + 1] != null) {
+                item2 = res.items[i+1];
+                title2 = item2.volumeInfo.title;
+                author2 = item2.volumeInfo.authors;
+                cover2 = (item2.volumeInfo.imageLinks) ? item2.volumeInfo.imageLinks.thumbnail : placeholder;
+                row += formatOutput(cover2, title2, author2);
+            }
+            
+            if (res.items[i + 2] != null) {
+                item3 = res.items[i+2];
+                title3 = item3.volumeInfo.title;
+                author3 = item3.volumeInfo.authors;
+                cover3 = (item3.volumeInfo.imageLinks) ? item3.volumeInfo.imageLinks.thumbnail : placeholder;
+                row += formatOutput(cover3, title3, author3);
 
-            item2 = res.items[i+1];
-            title2 = item2.volumeInfo.title;
-            author2 = item2.volumeInfo.authors;
-            cover2 = (item2.volumeInfo.imageLinks) ? item2.volumeInfo.imageLinks.thumbnail : placeholder;
-
-            item3 = res.items[i+2];
-            title3 = item3.volumeInfo.title;
-            author3 = item3.volumeInfo.authors;
-            cover3 = (item3.volumeInfo.imageLinks) ? item3.volumeInfo.imageLinks.thumbnail : placeholder;
-
-            item4 = res.items[i+3];
-            title4 = item4.volumeInfo.title;
-            author4 = item4.volumeInfo.authors;
-            cover4 = (item4.volumeInfo.imageLinks) ? item4.volumeInfo.imageLinks.thumbnail : placeholder;
-
-            item5 = res.items[i+4];
+            }
+            
+            if (res.items[i + 3] != null) {
+                item4 = res.items[i+3];
+                title4 = item4.volumeInfo.title;
+                author4 = item4.volumeInfo.authors;
+                cover4 = (item4.volumeInfo.imageLinks) ? item4.volumeInfo.imageLinks.thumbnail : placeholder;
+                row += formatOutput(cover4, title4, author4);
+            }
+            
+            row += '</div>';
+            outputList.innerHTML += row;
+            /*item5 = res.items[i+4];
             title5 = item5.volumeInfo.title;
             author5 = item5.volumeInfo.authors;
-            cover5 = (item5.volumeInfo.imageLinks) ? item5.volumeInfo.imageLinks.thumbnail : placeholder;
-            outputList.innerHTML += '<div id="row">' +
+            cover5 = (item5.volumeInfo.imageLinks) ? item5.volumeInfo.imageLinks.thumbnail : placeholder;*/
+            /*outputList.innerHTML += '<div id="row">' +
                                     formatOutput(cover1, title1, author1) +
                                     formatOutput(cover2, title2, author2) +
                                     formatOutput(cover3, title3, author3) +
                                     formatOutput(cover4, title4, author4) +
-                                    formatOutput(cover5, title5, author5) +
-                                    '</div>';
+                                    '</div>';*/
             console.log(outputList);
         }
     }
@@ -85,24 +99,6 @@ $(document).ready(function() {
                         </div>  
                     </div>`
         return card;
-        /*var col = document.createElement("div");
-        col.setAttribute("id", "col");
-        var img = document.createElement("img");
-        img.className = "book-cover";
-        img.src = "${cover}";
-        var about = document.createElement("div");
-        about.id = "book-about";
-        var bookTitle = document.createElement("h4");
-        bookTitle.className = "book-title";
-        bookTitle.innerHTML = "${title}";
-        var bookAuthor = document.createElement("h5");
-        bookAuthor.className = "book-author";
-        bookAuthor.innerHTML = "${author}";
-        about.appendChild(bookTitle);
-        about.appendChild(bookAuthor);
-        col.appendChild(about);
-        col.appendChild(img);
-        return col;*/
     }
 });
 
