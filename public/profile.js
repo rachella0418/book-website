d8afb6956f414af568e8f10708486c7c7b7a5759
Rachel La <rachel.la1807@gmail.com>
2023-07-12 16:46:34 -0400

    change username feature now works

--- public/profile.js
+++ public/profile.js
@@ -9,7 +9,7 @@ var changeUsernameBtn = document.getElementById("change-username");
 var cancelUsernameBtn = document.getElementById("cancel-username");
 var cancelPwBtn = document.getElementById("cancel-pw");
 var currPassword;
-
+var currUsername;
 
 // GET USER
 fetch("/user", {
@@ -21,6 +21,7 @@ fetch("/user", {
     fullname.value = data.user.name;
     username.value = data.user.username;
     currPassword = data.user.password;
+    currUsername = data.user.username;
     var temp = '.'.repeat(data.user.pwLength);
     password.value = temp;
 })
@@ -34,39 +35,35 @@ changeUsernameBtn.addEventListener("click", () => {
 })
 
 // SUBMIT NEW USERNAME
-submitUsernameBtn.addEventListener("click", () => {
+submitUsernameBtn.onclick = function() {
     var obj = {
+        currentPassword: "",
+        newPassword: "",
         newUsername: username.value,
+        newAvatar: "",
     };
-
     fetch("/update", {
         method: "POST",
-        headers: { "Content-type": "application/json" },
+        headers: {
+            "Content-type": "application/json"
+        },
         body: JSON.stringify(obj)
     }).then (response => {
         if (response.status === 200) {
-            disableChangingUsername();
+            disableEditMode(username, submitUsernameBtn, cancelUsernameBtn);
             console.log("username changed");
-        }
-        else if (response.status === 400) {
-            console.log("username existed, try again");
+        } else if (response.status === 400) {
+            console.log("username already existed");
         }
     }).catch(error => {
         console.log("error");
     })
-})
+}
 
 // CANCEL CHANGING USERNAME
 cancelUsernameBtn.addEventListener("click", () => {
-    disableChangingUsername();
+    disableEditMode(username, submitUsernameBtn, cancelUsernameBtn);
 })
-
-function disableChangingUsername() {
-    username.style.pointerEvents = "none";
-    username.style.border = "none";
-    submitUsernameBtn.style.visibility = "hidden";
-    cancelUsernameBtn.style.visibility = "hidden";
-}
 
 // CHANGE PASSWORD BUTTON
 changePwBtn.addEventListener("click", () => {
@@ -76,13 +73,63 @@ changePwBtn.addEventListener("click", () => {
     cancelPwBtn.style.visibility = "visible";
 })
 
+// SUBMIT NEW PASSWORD
+submitPwBtn.onclick = function() {
+    var obj = {
+        currentPassword: currPassword,
+        newPassword: password.value,
+        newUsername: "",
+        newAvatar: "",
+    };
+    fetch("/update", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(obj)
+    }).then (response => {
+        if (response.status === 200) {
+            disableEditMode(password, submitPwBtn, cancelPwBtn);
+            console.log("password changed");
+        }
+    }).catch(error => {
+        console.log("error");
+    })
+}
+/*submitPwBtn.addEventListener("click", () => {
+    var obj = {
+        currentPassword: currPassword,
+        newPassword: password.value,
+        newUsername: "",
+        newAvatar: "",
+    };
+    fetch("/update", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(obj)
+    }).then (response => {
+        if (response.status === 200) {
+            disableEditMode(password, submitPwBtn, cancelPwBtn);
+            console.log("password changed");
+        }
+    }).catch(error => {
+        console.log("error");
+    })
+})*/
+
 // CANCEL CHANGING PASSWORD
 cancelPwBtn.addEventListener("click", () => {
-    password.style.pointerEvents = "none";
-    password.style.border = "none";
-    submitPwBtn.style.visibility = "hidden";
-    cancelPwBtn.style.visibility = "hidden";
+    disableEditMode(password, submitPwBtn, cancelPwBtn);
 })
+
+function disableEditMode(variable, submit, cancel) {
+    variable.style.pointerEvents = "none";
+    variable.style.border = "none";
+    submit.style.visibility = "hidden";
+    cancel.style.visibility = "hidden";
+}
 
 // SIGN OUT
 signoutBtn.addEventListener("click", () => {
